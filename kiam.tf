f9dbe68d8ba146ed97046916988c132fbd552336
Alejandro Garrido Mota <garridomota@gmail.com>
2020-02-17 13:58:28 +0000

    Removed thanos (this will be in another PR)

--- kiam.tf
+++ kiam.tf
@@ -1,51 +1,51 @@
 
-##########
-# THANOS #
-##########
+# ##########
+# # THANOS #
+# ##########
 
-# This is to create a policy which allows Prometheus (thanos to be precise) to have a role to write to S3 without credentials
-data "aws_iam_policy_document" "monitoring_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
+# # This is to create a policy which allows Prometheus (thanos to be precise) to have a role to write to S3 without credentials
+# data "aws_iam_policy_document" "monitoring_assume" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "AWS"
-      identifiers = [var.iam_role_nodes]
-    }
-  }
-}
+#     principals {
+#       type        = "AWS"
+#       identifiers = [var.iam_role_nodes]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "monitoring" {
-  name               = "monitoring.${data.terraform_remote_state.cluster.outputs.cluster_domain_name}"
-  assume_role_policy = data.aws_iam_policy_document.monitoring_assume.json
-}
+# resource "aws_iam_role" "monitoring" {
+#   name               = "monitoring.${data.terraform_remote_state.cluster.outputs.cluster_domain_name}"
+#   assume_role_policy = data.aws_iam_policy_document.monitoring_assume.json
+# }
 
-data "aws_iam_policy_document" "monitoring" {
+# data "aws_iam_policy_document" "monitoring" {
 
-  statement {
-    actions = [
-      "s3:ListBucket",
-      "s3:GetObject",
-      "s3:DeleteObject",
-      "s3:PutObject"
-    ]
+#   statement {
+#     actions = [
+#       "s3:ListBucket",
+#       "s3:GetObject",
+#       "s3:DeleteObject",
+#       "s3:PutObject"
+#     ]
 
-    # Bucket name is hardcoded because it hasn't been created with terraform
-    # files inside this repository. Once we are happy with the test we must: 
-    # 1. Create S3 bucket from the cp-environments repo (or maybe from here?)
-    # 2. Use the output (S3 bucket name) in this policy
-    resources = [
-      "arn:aws:s3:::cloud-platform-2b5483a8020c73862c69b87f44c398b8/*",
-      "arn:aws:s3:::cloud-platform-2b5483a8020c73862c69b87f44c398b8"
-    ]
-  }
-}
+#     # Bucket name is hardcoded because it hasn't been created with terraform
+#     # files inside this repository. Once we are happy with the test we must: 
+#     # 1. Create S3 bucket from the cp-environments repo (or maybe from here?)
+#     # 2. Use the output (S3 bucket name) in this policy
+#     resources = [
+#       "arn:aws:s3:::cloud-platform-2b5483a8020c73862c69b87f44c398b8/*",
+#       "arn:aws:s3:::cloud-platform-2b5483a8020c73862c69b87f44c398b8"
+#     ]
+#   }
+# }
 
-resource "aws_iam_role_policy" "monitoring" {
-  name   = "route53"
-  role   = aws_iam_role.monitoring.id
-  policy = data.aws_iam_policy_document.monitoring.json
-}
+# resource "aws_iam_role_policy" "monitoring" {
+#   name   = "route53"
+#   role   = aws_iam_role.monitoring.id
+#   policy = data.aws_iam_policy_document.monitoring.json
+# }
 
 ###########
 # Grafana #
