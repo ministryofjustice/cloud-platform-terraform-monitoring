d55ce939c1a56c2faa463caa1708379fedc2cf5c
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-11-10 12:33:42 +0000

    Remove localstack terraform unit tests and do terraform init and validate

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -1,38 +1,7 @@
 provider "aws" {
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
-  region                      = "eu-west-2"
-  s3_force_path_style         = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    apigateway     = "http://localhost:4566"
-    cloudformation = "http://localhost:4566"
-    cloudwatch     = "http://localhost:4566"
-    dynamodb       = "http://localhost:4566"
-    ec2            = "http://localhost:4566"
-    es             = "http://localhost:4566"
-    elasticache    = "http://localhost:4566"
-    firehose       = "http://localhost:4566"
-    iam            = "http://localhost:4566"
-    kinesis        = "http://localhost:4566"
-    lambda         = "http://localhost:4566"
-    rds            = "http://localhost:4566"
-    redshift       = "http://localhost:4566"
-    route53        = "http://localhost:4566"
-    s3             = "http://localhost:4566"
-    secretsmanager = "http://localhost:4566"
-    ses            = "http://localhost:4566"
-    sns            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
-    ssm            = "http://localhost:4566"
-    stepfunctions  = "http://localhost:4566"
-    sts            = "http://localhost:4566"
-  }
+  region  = "eu-west-2"
+  profile = "moj-cp"
 }
-
 module "prometheus" {
   source = "../../"
 
@@ -43,13 +12,15 @@ module "prometheus" {
       channel  = "#lower-priority-alarms"
   }]
 
-  iam_role_nodes                             = "arn:mogaal"
+  iam_role_nodes                             = "arn:cloud-platform"
   pagerduty_config                           = "asdasd"
   enable_ecr_exporter                        = false
   enable_cloudwatch_exporter                 = false
   enable_thanos_helm_chart                   = false
   enable_thanos_sidecar                      = false
   enable_prometheus_affinity_and_tolerations = false
+  enable_large_nodesgroup                    = false
+  enable_thanos_compact                      = false
 
   cluster_domain_name           = "prometheus.cloud-platform.service.justice.gov.uk"
   oidc_components_client_id     = "XXX"
