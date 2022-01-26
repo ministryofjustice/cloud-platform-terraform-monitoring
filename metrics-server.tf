a8a560de150c95a3f06bd839363329ebdfd0078a
PaulWyborn <paul.wyborn@digital.justice.gov.uk>
2022-01-26 11:04:52 +0000

    config change

--- metrics-server.tf
+++ metrics-server.tf
@@ -1,5 +1,5 @@
 
-resource "helm_release" "metrics_server" {
+resource "helm_release" "metrics-server" {
   name       = "metrics-server"
   chart      = "metrics-server"
   repository = "https://charts.bitnami.com/bitnami"
