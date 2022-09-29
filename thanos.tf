34d17dfcaf531a1a6762d49991ac2632e1e5297b
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2022-09-29 13:51:02 +0100

    feat(thanos): Add storageClass to allow snapshot encryption

--- thanos.tf
+++ thanos.tf
@@ -17,6 +17,7 @@ resource "helm_release" "thanos" {
     enabled_compact     = var.enable_thanos_compact
     monitoring_aws_role = module.iam_assumable_role_monitoring.this_iam_role_name
     clusterName         = terraform.workspace
+		storage_class				= "gp2-expand"
   })]
 
   depends_on = [
