3da1ef25923d6876f12bbde7f7cf4bf5f06dc9eb
Emterry <emma.terry@digital.justice.gov.uk>
2025-12-03 12:11:47 +0000

    test proxy

--- prometheus.tf
+++ prometheus.tf
@@ -36,7 +36,7 @@ resource "helm_release" "prometheus_operator_eks" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
   namespace  = kubernetes_namespace.monitoring.id
-  version    = "79.4.1"
+  version    = "69.8.2"
   skip_crds  = true # Crds are managed separately using resource kubectl_manifest.prometheus_operator_crds in core
   timeout    = 600
 
